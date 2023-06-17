1f43fa5049c655b54116d945674c24b4535188dc
Alex Nyabok <alenyabok@gmail.com>
2023-06-17 10:48:41 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -30,7 +30,7 @@ int main(void)
         }
         else if (lst_dgt < 6 && lst_dgt != 0)
         {
-        printf("The last digit of %d is %d and is less than 6 and not 0\n", n, lst_dgt);
+        printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lst_dgt);
         }
         return (0);
 }
