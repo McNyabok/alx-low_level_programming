6a3b7745ee00c98b33913fb6ee5d80ced43d71df
Alex Nyabok <alenyabok@gmail.com>
2023-06-17 10:43:38 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,16 +18,17 @@ int main(void)
 
         srand(time(0));
         n = rand() - RAND_MAX / 2;
+	lst_dgt = n % 10;
 
         if (lst_dgt > 5)
         {
         printf("Last digit of %d is %d and is greater than 5\n", n, lst_dgt);
         }
-        else if (n == 0)
+        else if (lst_dgt == 0)
         {
         printf("Last digit of %d is %d and is 0\n", n, lst_dgt);
         }
-        else if (n < 6 && n != 0)
+        else if (lst_dgt < 6 && lst_dgt != 0)
         {
         printf("The last digit of %d is %d and is less than 6 and not 0\n", n, lst_dgt);
         }
