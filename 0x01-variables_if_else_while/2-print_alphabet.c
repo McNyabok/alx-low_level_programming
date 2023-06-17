06000eeaf4e3bfdb52699fdf736e06fc6cece470
Alex Nyabok <alenyabok@gmail.com>
2023-06-17 12:22:28 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ * Description:prints the alphabet in lowercase using 'putchar' function
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char alph_lower;
+
+	for (alph_lower = 'a' ; alph_lower <= 'z' ; alph_lower++)
+	putchar(alph_lower);
+	putchar('\n ');
+	return (0);
+}
