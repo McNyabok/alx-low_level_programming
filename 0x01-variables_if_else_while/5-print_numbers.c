b539a6a041c679bc096353c2be216472e356f163
Alex Nyabok <alenyabok@gmail.com>
2023-06-18 00:00:05 -0700

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -10,7 +10,8 @@ int main(void)
 {
 	int single_base10_dgtNums;
 
-	for (single_base10_dgtNums = '0'; single_base10_dgtNums <='9'; single_base10_dgtNums++)
+	for (single_base10_dgtNums = '0';
+	single_base10_dgtNums <= '9'; single_base10_dgtNums++)
 	printf("%d\n", single_base10_dgtNums);
 	return (0);
 }
