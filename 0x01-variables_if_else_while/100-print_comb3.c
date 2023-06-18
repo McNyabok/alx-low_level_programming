1a5075577dc6c1c6c4866276a7684c39357b9e46
Alex Nyabok <alenyabok@gmail.com>
2023-06-18 03:26:58 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,30 +11,27 @@
  */
 
 int main(void)
-        
-{       
-        int Num1;
-        int Num2;
-        
-        for (Num1 = '0'; Num1 <= '9'; Num1++)
-        {
-        for (Num2 = '0'; Num2 <= '9'; Num2++)
-        {
-        if (Num1 != Num2)
-        {      
-        putchar(Num1);
-        putchar(Num2);
-        
-        if (Num1 == '8' && Num2 == '9')
-        continue;
-                
-        putchar(',');
-        putchar(' ');
-        }
-        }
-        }
-        putchar('\n');
-                
-        return (0);
+{
+
+	int Num1;
+	int Num2;
+	
+	for (Num1 = '0'; Num1 <= '9'; Num1++)
+	{
+	for (Num2 = '0'; Num2 <= '9'; Num2++)
+	{
+	if (Num1 != Num2)
+	{      
+	putchar(Num1);
+	putchar(Num2);
+	if (Num1 == '8' && Num2 == '9')
+	continue;
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	}
+	putchar('\n');
+	return (0);
         
 } 
