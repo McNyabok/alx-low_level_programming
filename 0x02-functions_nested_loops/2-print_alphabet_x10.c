4e693ff64236acec379e64592b2de366e6a4fe42
Alex Nyabok <alenyabok@gmail.com>
2023-06-20 08:02:17 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print alphabets_x10 - prints 10 times the lowercase alphabets,
+ * followed by a new line
+ *
+ * Return: void
+ */
+
+void print_alphabet_x10(void);
+
+{
+	char alphs; 
+	int i = 0;
+
+	while (i < '10'){
+
+		for (alphs = 'a'; alphs <= 'z'; alphs++){
+			_putchar(alphs);
+		}
+		_putchar('\n');
+		i++
+	}
+}
