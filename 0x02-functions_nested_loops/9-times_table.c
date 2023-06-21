73bb0b036bbf82e473cba493682cc227e41ed207
Alex Nyabok <alenyabok@gmail.com>
2023-06-21 04:14:26 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -25,6 +25,8 @@ void times_table(void)
 	_putchar(e + '0');
 	_putchar(d + '0');
 	}
+	else
+	{
 	if (b != 0)
 	{
 	_putchar(44);
@@ -35,4 +37,5 @@ void times_table(void)
 	}
 	}
 	_putchar('\n');
+	}
 }
