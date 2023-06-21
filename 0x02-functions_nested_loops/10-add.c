7217b712d09366bd45779e0c83ae7c35ca13c7a5
Alex Nyabok <alenyabok@gmail.com>
2023-06-21 04:33:41 -0700

    10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,7 +2,8 @@
 
 /**
  * add - adds two integers and returns the result.
- *
+ * @i: first integer to be added
+ * @j: second integer to be added
  * Return: result
  */
 
@@ -10,7 +11,7 @@ int add(int i, int j)
 
 {
 	int result = i + j;
+
+
 	return (result);
 }
-
-
