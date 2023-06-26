1f95af547e51608a09d5996ae5759b201195842f
Alex Nyabok <alenyabok@gmail.com>
2023-06-26 05:26:33 -0700

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -5,11 +5,11 @@
  *
  * Return: string length
  */
-int _strlen(char *s)
+size_t _strlen(const char *str)
 {
-	int len = 0;
+	size_t len = 0;
 
-	while (*s)
+	while (*str++)
 	len++;
 	return (len);
 }
