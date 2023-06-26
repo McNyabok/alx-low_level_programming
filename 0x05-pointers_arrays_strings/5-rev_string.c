44f1c523193dc820aa40e746ad542522a61c7cfe
Alex Nyabok <alenyabok@gmail.com>
2023-06-26 06:16:57 -0700

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * rev_string -  reverses a string
+ *
+ * @s: string to reverse
+ *
+ * return: void
+ */
+void rev_string(char *s)
+{
+	int length = 0;
+	int index = 0;
+	char c;
+
+		while (s[index++])
+			length+=;
+		for (index = length - 1; index >= length / 2; index--)
+		{
+			c = s[index];
+			s[index] = s[length - index - 1];
+			s[length - index - 1] = c;
+		}
+}
